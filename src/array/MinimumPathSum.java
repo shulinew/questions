f11b3bf6a202c04d48e84510745723f8dfc14cf3
shuli <shuli@ebay.com>
2015-12-03 10:15:44 -0800

    Add using linear sapce

--- src/array/MinimumPathSum.java
+++ src/array/MinimumPathSum.java
@@ -23,5 +23,25 @@ public class MinimumPathSum {
     	return grid[n-1][m-1];
         
     }
+    
+    //Only use linear space
+    public int minPathSum1(int[][] grid) {
+    	int m = grid[0].length;
+    	int [] cache = new int[m];
+    	cache[0] = grid[0][0];
+    	for (int i = 1; i< grid.length;i++){
+    		cache[i] = cache[i-1] + grid[i][0];
+    	}
+    	for (int j = 1; j< m;j++){
+    		grid[0][j] = grid[0][j-1] + grid[0][j];
+    	}
+    	for (int i = 1;i< n;i++){
+    		for (int j =1; j< m;j++){
+    			grid[i][j] = grid[i][j] + Math.min(grid[i][j-1], grid[i-1][j]);
+    		}
+    	}
+    	return grid[n-1][m-1];
+        
+    }
 
 }

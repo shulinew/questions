4a5520811131a9e3b6002085b254901405197873
shuli <shuli@ebay.com>
2015-12-14 17:52:33 -0800

    Add recursive method

--- src/array/SubSet.java
+++ src/array/SubSet.java
@@ -62,6 +62,28 @@ public class SubSet {
     	}
     	return subsets;
     }
+    public List<List<Integer>> subsets3(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if(nums.length == 0){
+            return result;
+        }
+
+        Arrays.sort(nums);
+        dfs(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    public void dfs(int[] s, int index, List<Integer> path, List<List<Integer>> result){
+        result.add(new ArrayList<Integer>(path));
+
+        for(int i = index; i < s.length; i++){
+            path.add(s[i]);
+            dfs(s, i+1, path, result);
+            path.remove(path.size()-1);
+        }
+    }
+
     
     /*
      * Given a collection of integers that might contain duplicates, nums, 
